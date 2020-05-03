403dcfcd76f4c277e0a908897860adbdc244fcef
Penghnlab <50325686+plhhnu@users.noreply.github.com>
2020-05-03 11:09:46 +0800

    Update nrlmf.py

--- nrlmf.py
+++ nrlmf.py
@@ -181,7 +181,7 @@ class NRLMF:
         #scores = scores.tolist()
         acc, spec = computation(test_label, scores)
         scores = np.array(scores)
-        sen = recall_score(test_label, (scores > 0.6).astype(int), average='micro')
+        sen = recall_score(test_label, (scores > 0.5).astype(int), average='micro')
 
         return aupr_val, auc_val, acc, sen, spec
 
